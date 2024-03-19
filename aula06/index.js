11f18c7a3f90f8dc01c5eefbbf21d4c8e5fc5509
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-19 18:29:18 -0300

    Update index.js

--- aula06/index.js
+++ aula06/index.js
@@ -13,3 +13,10 @@ console.log(nome);
 // As variáveis precisam ter nomes significativos
 
 // ex: let n = 'João';
+
+// Não pode começar um nome de variável com um número
+
+// Ex: let 1nome = 'seila'
+
+// Não podem conter espaços ou traço
+// ex: let nome-cliente = 'seila'
