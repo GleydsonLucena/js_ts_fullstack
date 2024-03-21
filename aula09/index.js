ec01908f65df4f7df4854459472a3bf6547ccf3b
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-21 18:01:30 -0300

    Update index.js

--- aula09/index.js
+++ aula09/index.js
@@ -1,3 +1,7 @@
 const nome = 'Gleydson';
 const nome1 = "Luiz";
 const nome2 = `Luiz`
+const num = 10; // Number
+const num1 = 10.52; // Number
+let nomeAluno; // undefined = Não aponta pra lugar nenhum
+let sobrenomeAluno = null; // nulo = não aponta pra lugar nenhum na memória
