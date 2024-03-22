59733c241bcd5bf825d9b9f2307039c6c01369c2
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-22 17:42:45 -0300

    Atualização

--- aula11 copy/js/script.js
+++ aula11 copy/js/script.js
@@ -3,6 +3,6 @@ let num2 = prompt('Digite outro número:');
 
 Number(num2, num1)
 
-let calculo = num1 **num2;
+let calculo = num1 ** num2;
 
 alert(calculo)
--- exercicios/ex001.js
+++
@@ -1,3 +0,0 @@
-console.log('Olá, meu nome é "Gleydson". Estou aprendendo JavaScript às ' + 10 + ' da manhã.');
-
-console.log('Este é um número ', 10000);
