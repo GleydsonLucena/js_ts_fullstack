4ebff153e5685c760aac0dac9ba229731933b235
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-22 17:37:26 -0300

    Update script.js

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,6 +1,8 @@
 let num1 = prompt('Digite um número:');
 let num2 = prompt('Digite outro número:');
 
-let calculo = Number(num1) + Number(num2);
+Number(num2, num1)
+
+let calculo = num1 + num2;
 
 alert(calculo)
