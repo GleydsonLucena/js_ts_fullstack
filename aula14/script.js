f1b906bf2b4d969688ec51321e035e1c583b20ea
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-25 17:39:09 -0300

    Update script.js

--- aula14/script.js
+++ aula14/script.js
@@ -1,5 +1,10 @@
-let num1 = 1;
+let num1 = 10;
 let num2 = 2.5;
 
-console.log(num1.toString() + num2);
-console.log(typeof num1);
+// console.log(num1.toString() + num2);
+
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));
+let temp = num1 * 'Olá'; 
+console.log(temp)
