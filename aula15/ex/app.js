4919f8f890d84d2dfb95f73ced391ee368011072
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-25 19:30:33 -0300

    Atualização

---
+++ aula15/ex/app.js
@@ -0,0 +1,14 @@
+let num = prompt('Digte um número:');
+
+num = Number(num);
+
+const h1 = document.getElementById('tittle');
+const p = document.getElementById('parag');
+
+h1.innerHTML += `Seu número é ${num} <br />`
+p.innerHTML += `Raiz Quadrada = ${(num ** 0.5).toFixed(2)} <br />`
+p.innerHTML += `${num} é Inteiro? ${Number.isInteger(num)} <br />`
+p.innerHTML += `NaN? ${Number.isNaN(num)} <br />`
+p.innerHTML += `Arredondando para baixo: ${Math.floor(num)} <br />`
+p.innerHTML += `Arredondando para cima: ${Math.ceil(num)} <br />`
+p.innerHTML += `Arredondando para cima: ${Math.ceil(num)} <br />`
--- aula15/script.js
+++ aula15/script.js
@@ -1,7 +1,14 @@
-let num1 = 9.546486
+// let num1 = 9.546486
 
 // let num2 = Math.floor(num1);
 // let num2 = Math.ceil(num1);
-let num2 = Math.round(num1);
+// let num2 = Math.round(num1);
 
-console.log(num2);
+// console.log(Math.max(1,5,456,456,12,1212,12));
+// console.log(Math.min(1,5,456,456,12,1212,12));
+// const rand = Math.random()
+
+let num1 = 0;
+
+console.log(100 / 0); 
+
