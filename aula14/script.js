33fe2a71e8eb22abba08ec1bb9e147ddca275ba6
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-25 18:37:05 -0300

    Update script.js

--- aula14/script.js
+++ aula14/script.js
@@ -1,10 +1,18 @@
-let num1 = 10;
-let num2 = 2.5;
+let num1 = 5;
+let num2 = '5';
+let resultado;
 
 // console.log(num1.toString() + num2);
 
 // console.log(num1.toString(2));
 // console.log(num1.toFixed(2));
 // console.log(Number.isInteger(num1));
-let temp = num1 * '5'; 
-console.log(Number.isNaN(temp));
+resultado = num1 + num2;
+if (Number.isNaN(resultado)) {
+    console.log('Isso n é um número');
+} else if (typeof num2 === string) {
+    Number(num1)
+}  {
+    console.log(resultado);
+}
+
