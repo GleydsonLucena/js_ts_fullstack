3a6412f4fcaa0e028ff070051cb5c4c9b9242dd4
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-25 18:17:26 -0300

    Update script.js

--- aula14/script.js
+++ aula14/script.js
@@ -6,5 +6,5 @@ let num2 = 2.5;
 // console.log(num1.toString(2));
 // console.log(num1.toFixed(2));
 // console.log(Number.isInteger(num1));
-let temp = num1 * 'Olá'; 
-console.log(temp)
+let temp = num1 * '5'; 
+console.log(Number.isNaN(temp));
