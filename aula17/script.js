fcf988739d8d3872da90f4f724d8319ee6cf5bea
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-28 14:37:37 -0300

    Update script.js

---
+++ aula17/script.js
@@ -0,0 +1,9 @@
+function saudação(nome) {
+    return `Bom dia! ${nome}`;
+}
+let array = ['Gleydson', 'Maria', 'Carlos', 'João'];
+
+for (let i = 0; i < array.length; i++) {
+    saudação(array[i]);
+    
+}
