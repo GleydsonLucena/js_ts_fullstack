dd03182cf2e5daa5bc615931673832c3f3a599b9
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-28 14:25:45 -0300

    Create script.js

---
+++ aula17/script.js
@@ -0,0 +1,16 @@
+let nomes = ['Gleydson', 'Luiz', 'Maria', 'Pedro'];
+// console.log(nomes.push('Eduardo'))
+// console.log(nomes.unshift('Otavio'))
+// console.log(nomes.unshift('Flávio'))
+// const removido = nomes.shift();
+// console.log(nomes);
+
+// nomes.push('Luiza');
+// nomes.push('Eduardo');
+
+
+
+console.log(nomes.slice(3, -1))
+nomes = 133;
+
+console.log(nomes instanceof Array);
