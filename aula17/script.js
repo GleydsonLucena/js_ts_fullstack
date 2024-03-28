51e1db271c939b087f17f36f70bcfd7647e2a131
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-28 14:39:13 -0300

    Update script.js

--- aula17/script.js
+++ aula17/script.js
@@ -1,9 +1,16 @@
-function saudação(nome) {
-    return `Bom dia! ${nome}`;
-}
-let array = ['Gleydson', 'Maria', 'Carlos', 'João'];
+// function saudação(nome) {
+//     return `Bom dia! ${nome}`;
+// }
+// let array = ['Gleydson', 'Maria', 'Carlos', 'João'];
 
-for (let i = 0; i < array.length; i++) {
-    saudação(array[i]);
+// for (let i = 0; i < array.length; i++) {
+//     saudação(array[i]);
     
+// }
+let resultado;
+function soma(a, b) {
+    resultado = a * b;
+    return resultado;
 }
+
+console.log(soma(1,5));
