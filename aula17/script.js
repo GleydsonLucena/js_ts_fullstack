de7ae3463e8fb245ee6f66ffcbf69acdfd0c24bc
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-28 14:53:07 -0300

    Update script.js

--- aula17/script.js
+++ aula17/script.js
@@ -16,8 +16,12 @@
 // console.log(soma(1,5)); 
 // console.log(resultado)
 
-const nome = function (n) {
-    return n ** 0.5;
-};
+// const nome = function (n) {
+//     return n ** 0.5;
+// };
 
-console.log(nome(9))
+// console.log(nome(9))
+
+const nome = n => n ** 0.5;
+
+console.log(nome(9));
