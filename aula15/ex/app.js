e4549334b9ca64b0f0c3e822f4deac4e886ba187
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-27 21:19:40 -0300

    Update app.js

--- aula15/ex/app.js
+++ aula15/ex/app.js
@@ -1,6 +1,4 @@
-let num = prompt('Digte um número:');
-
-num = Number(num);
+let num = Number(prompt('Digte um número:'));
 
 const h1 = document.getElementById('tittle');
 const p = document.getElementById('parag');
@@ -10,6 +8,5 @@ p.innerHTML += `Raiz Quadrada = ${(num ** 0.5).toFixed(2)} <br />`;
 p.innerHTML += `${num} é Inteiro? ${Number.isInteger(num)} <br />`;
 p.innerHTML += `NaN? ${Number.isNaN(num)} <br />`;
 p.innerHTML += `Arredondando para baixo: ${Math.floor(num)} <br />`;
-p.innerHTML += `Arredondando para cima: ${Math.ceil(num)} <br />`;
 p.innerHTML += `Arredondando para cima: ${Math.ceil(num)} <br />`;
 p.innerHTML += `${num} com duas casas decimais: ${num.toFixed(2)} <br />`;
