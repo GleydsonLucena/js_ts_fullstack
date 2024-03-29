d1b9e30a60170f73ad953843da21b8c0263069ff
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-29 19:11:30 -0300

    Update script.js

--- aula19/script.js
+++ aula19/script.js
@@ -3,9 +3,21 @@ tipos de dados primitivos - string, Number, boolen, underfined,
 null (biging, symbol)
 */
 
-let a ='A';
+// let a ='A';
+// let b = a;
+// console.log(a, b);
+
+// a = 'outra coisa';
+// console.log(a, b);
+
+let a = [1, 2, 3];
 let b = a;
+
 console.log(a, b);
 
-a = 'outra coisa';
+a.push(6);
+console.log(a,b);
+
+b.pop();
+
 console.log(a, b);
