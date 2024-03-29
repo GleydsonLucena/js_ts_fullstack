6c5b9292cc792811495de9da32f5b1b072538704
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-29 18:08:43 -0300

    Update script.js

--- aula18/script.js
+++ aula18/script.js
@@ -1,27 +1,17 @@
-// function saudação(nome) {
-//     return `Bom dia! ${nome}`;
-// }
-// let array = ['Gleydson', 'Maria', 'Carlos', 'João'];
+// const array = [1, 2, 3];
+// array.push(4);
+// console.log(array);
+// array[0] = ('Luiz');
+// console.log(array);
 
-// for (let i = 0; i < array.length; i++) {
-//     saudação(array[i]);
-    
-// }
-// let resultado;
-// function soma(a=0, b=0) {
-//     resultado = Number(a * b);
-//     return resultado;
-// }
 
-// console.log(soma(1,5)); 
-// console.log(resultado)
+const nome1 = 'Gleydson';
+const sobrenome = 'Lucena';
+const idade = 25;
 
-// const nome = function (n) {
-//     return n ** 0.5;
-// };
 
-// console.log(nome(9))
+const nome2 = 'Luiz';
+const sobrenome2 = 'Miranda';
+const idade3 = 55;
 
-const nome = n => n ** 0.5;
-
-console.log(nome(9));
+console.log()
