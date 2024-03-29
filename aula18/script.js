cf6d83847795f33b20f8922863d65cd07144f984
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-29 18:04:41 -0300

    Create script.js

---
+++ aula18/script.js
@@ -0,0 +1,27 @@
+// function saudação(nome) {
+//     return `Bom dia! ${nome}`;
+// }
+// let array = ['Gleydson', 'Maria', 'Carlos', 'João'];
+
+// for (let i = 0; i < array.length; i++) {
+//     saudação(array[i]);
+    
+// }
+// let resultado;
+// function soma(a=0, b=0) {
+//     resultado = Number(a * b);
+//     return resultado;
+// }
+
+// console.log(soma(1,5)); 
+// console.log(resultado)
+
+// const nome = function (n) {
+//     return n ** 0.5;
+// };
+
+// console.log(nome(9))
+
+const nome = n => n ** 0.5;
+
+console.log(nome(9));
