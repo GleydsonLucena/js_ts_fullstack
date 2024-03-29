f741e260326836f67160833e254ed5de76849a99
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-29 18:11:47 -0300

    Update script.js

--- aula18/script.js
+++ aula18/script.js
@@ -14,4 +14,19 @@ const nome2 = 'Luiz';
 const sobrenome2 = 'Miranda';
 const idade3 = 55;
 
-console.log()
+const pessoa1 = {
+    nome: 'Gleydson',
+    sobrenome: 'Lucena', 
+    idade: 23
+}
+const pessoa2 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda', 
+    idade: 55
+}
+
+const array = [pessoa1, pessoa2];
+
+console.log(array);
+
+
