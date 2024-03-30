38dc6c9bea300a1434e66e45b3cebe5d68344550
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-30 20:56:36 -0300

    Update script.js

--- aula21/script.js
+++ aula21/script.js
@@ -1,5 +1,10 @@
 const expressao = 10 > 5;
 const comp = 10 >= 11;
 
+
+let num1 = 10;
+let num2 = '10';
+const compara = num1 === num2;
 console.log(expressao);
 console.log(comp);
+console.log(compara);
