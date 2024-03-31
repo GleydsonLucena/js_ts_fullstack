f2efcb79e30e5f74e8c2c2047d2c724226a6616d
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-30 21:27:15 -0300

    Update script.js

--- aula22/script.js
+++ aula22/script.js
@@ -1,10 +1,8 @@
-const expressao = 10 > 5;
-const comp = 10 >= 11;
+// Operadores lógicos 
+
+// && AND - E
+// || OR - OU
+// ! NOT - NÃO
 
 
-let num1 = 10;
-let num2 = '10';
-const compara = num1 === num2;
-console.log(expressao);
-console.log(comp);
-console.log(compara);
+console.log(10 > 5 && 15 > 12);
