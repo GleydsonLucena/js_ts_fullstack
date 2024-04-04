90748503342960ed98a7a5cab0408ece0af1b943
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-04 17:05:42 -0300

    Update script.js

--- aula22/script.js
+++ aula22/script.js
@@ -1,10 +1,10 @@
-// Operadores lógicos
+// Operadores lógicos 
 
 // && AND - E (As duas expressoes tem que ser verdadeira)
 // || OR - OU (Um das expressoes apenas precisa ser verdadeira)
-// ! NOT - NÃO
+// ! NOT - NÃO 
 
-// const expressaoAnd = true && true && true && true;
+// const expressaoAnd = true && true && true && true; 
 // const expressaoOr = false || false;
 //  console.log(expressaoAnd, expressaoOr);
 
