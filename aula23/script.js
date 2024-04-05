105640a462fe0472303c8189b059671e64079732
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-05 09:27:15 -0300

    Update script.js

--- aula23/script.js
+++ aula23/script.js
@@ -6,3 +6,12 @@
 
 // console.log(vaiExecultar && falarOi());
 
+// console.log(0 || null || 'Gleydson' || true);
+
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e);
