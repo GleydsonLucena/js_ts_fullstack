339ed805fcfa410514c735c708771607a9a63469
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-05 09:44:01 -0300

    Update script.js

--- aula24/script.js
+++ aula24/script.js
@@ -1,9 +1,9 @@
-const hora = 19;
+const hora = 23;
 
-if (hora > 12) {
-    console.log('Boa tarde');
-} else if ( hora >= 18) {
-    console.log('Boa noite');
-} else {
+if (hora >= 0 && hora <= 11) {
     console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde');
+} else {
+    console.log('Boa noite');
 }
