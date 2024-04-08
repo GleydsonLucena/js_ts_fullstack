28542d20a23ac5930a28cfb832a82633570c6851
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-08 18:55:56 -0300

    atualização

---
+++ ex/ex_ IMC/assets/js/main.js
@@ -0,0 +1,15 @@
+const meuEscopo = () => {
+    const peso = document.getElementById('txtpeso');
+    const altura = document.getElementById('txtaltura');
+    const botao = document.querySelector('.btn');
+    
+    botao.addEventListener('click', msg=()=> {
+        
+        console.log()
+    })
+    
+}
+
+meuEscopo()
+
+
