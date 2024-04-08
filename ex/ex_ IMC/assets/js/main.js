c04093f921d0cbcf64b4f45bad346dfbab697226
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-08 20:02:44 -0300

    Update main.js

--- ex/ex_ IMC/assets/js/main.js
+++ ex/ex_ IMC/assets/js/main.js
@@ -6,10 +6,52 @@ form.addEventListener('submit', function pararSubmit (event) {
 
 const btn = document.querySelector('.btn');
 
+function exibirResultado (imc) {
+    const resultado = document.querySelector('.resultado');
+
+    resultado.style.display = 'inline';
+
+    if (imc <= 18.5) {
+        resultado.innerHTML = `Seu IMC é ${imc} => Está abaixo do peso.`;
+        resultado.style.background = 'yellow';
+    } else if (imc >= 18.6 && imc <= 24,9) {
+        resultado.innerHTML = `Seu IMC é ${imc} => Peso normal.`;
+        resultado.style.background = 'green';
+    } else if (imc >= 25 && imc <= 29.9) {
+        resultado.innerHTML = `Seu IMC é ${imc} => Sobrepeso.`;
+        resultado.style.background = 'red';
+    } else if (imc >= 30 && imc <= 39.9) {
+        resultado.innerHTML = `Seu IMC é ${imc} => Obesidade grau 1.`;
+        resultado.style.background = 'red';
+    } else if (imc >= 35 && imc < 40) {
+        resultado.innerHTML = `Seu IMC é ${imc} => Obesidade grau 2.`;
+        resultado.style.background = 'red';
+    } else if (imc > 40) {
+        resultado.innerHTML = `Seu IMC é ${imc} => Obesidade grau 3.`;
+        resultado.style.background = 'red';
+    }
+
+}
+
+function calcularImc(peso, altura) {
+    let calc = peso / (altura * altura)
+    return calc.toFixed(2);
+}
+
 btn.addEventListener('click', function cliqueiCalcular () {
     const peso = Number(document.querySelector('#txtpeso').value);
     const altura = Number(document.querySelector('#txtaltura').value);
     
-    console.log(peso / (altura ** 1/2))
+    
+    exibirResultado(calcularImc(peso, altura))
+    
 })
+
+
+
+
+
+
+
+
 
