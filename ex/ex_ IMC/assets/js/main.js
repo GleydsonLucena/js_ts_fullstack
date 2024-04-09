5d7475366ab298f81341547d55def65c772d3b98
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-09 17:52:21 -0300

    Refazendo a atividade de IMC

--- ex/ex_ IMC/assets/js/main.js
+++ ex/ex_ IMC/assets/js/main.js
@@ -1,57 +1,26 @@
-const form = document.querySelector('form');
+const form = document.querySelector(".form");
+const botao = document.querySelector(".btn");
 
-form.addEventListener('submit', function pararSubmit (event) {
-   event.preventDefault(); 
-} )
+// FUNÇÕES
 
-const btn = document.querySelector('.btn');
-
-function exibirResultado (imc) {
-    const resultado = document.querySelector('.resultado');
-
-    resultado.style.display = 'inline';
-
-    if (imc <= 18.5) {
-        resultado.innerHTML = `Seu IMC é ${imc} => Está abaixo do peso.`;
-        resultado.style.background = 'yellow';
-    } else if (imc >= 18.6 && imc <= 24,9) {
-        resultado.innerHTML = `Seu IMC é ${imc} => Peso normal.`;
-        resultado.style.background = 'green';
-    } else if (imc >= 25 && imc <= 29.9) {
-        resultado.innerHTML = `Seu IMC é ${imc} => Sobrepeso.`;
-        resultado.style.background = 'red';
-    } else if (imc >= 30 && imc <= 39.9) {
-        resultado.innerHTML = `Seu IMC é ${imc} => Obesidade grau 1.`;
-        resultado.style.background = 'red';
-    } else if (imc >= 35 && imc < 40) {
-        resultado.innerHTML = `Seu IMC é ${imc} => Obesidade grau 2.`;
-        resultado.style.background = 'red';
-    } else {
-        resultado.innerHTML = `Seu IMC é ${imc} => Obesidade grau 3.`;
-        resultado.style.background = 'red';
-    }
-
+function calcularImc (peso, altura) {
+    const imc = peso / (altura * altura);
+    return imc.toFixed(1)
 }
 
-function calcularImc(peso, altura) {
-    let calc = peso / (altura * altura)
-    return calc.toFixed(2);
-}
+// EVENTOS
 
-btn.addEventListener('click', function cliqueiCalcular () {
-    const peso = Number(document.querySelector('#txtpeso').value);
-    const altura = Number(document.querySelector('#txtaltura').value);
-    
-    
-    exibirResultado(calcularImc(peso, altura))
-    
+form.addEventListener("submit", previnirSubmit = (event) => {
+    event.preventDefault();
 })
 
+botao.addEventListener("click", cliqueiCalcular = () => {
+    
+    const inputPeso = document.querySelector("#txtpeso");
+    const inputAltura = document.querySelector("#txtaltura");
 
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
 
-
-
-
-
-
-
+    console.log(calcularImc(peso, altura));
+})
