7abd1aa98218594297abcdcb657e717ce54b21a1
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-10 15:42:17 -0300

    Update script.js

--- aula27/script.js
+++ aula27/script.js
@@ -11,3 +11,8 @@ const pontuacaoUsuario = 1000;
 const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
 
 console.log(nivelUsuario)
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao)
