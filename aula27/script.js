9f6f63b151e68e52f0dfb28881cab0574a13c213
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-10 15:40:15 -0300

    Update script.js

--- aula27/script.js
+++ aula27/script.js
@@ -1,7 +1,13 @@
-const pontuacaoUsuario = 999;
+const pontuacaoUsuario = 1000;
 
-if (pontuacaoUsuario >= 1000) {
-    console.log('Usuário VIP');
-} else {
-    console.log('Usuário normal');
-}
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('Usuário VIP');
+// } else {
+//     console.log('Usuário normal');
+// }
+
+// (condição) ? 'valor verdadeiro' : 'valor falso';
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+console.log(nivelUsuario)
