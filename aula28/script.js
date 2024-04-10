15ba90cac13ae7cfb9a0422e3cb0d97507816bf2
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-10 16:53:51 -0300

    Update script.js

--- aula28/script.js
+++ aula28/script.js
@@ -2,20 +2,48 @@ const data = new Date();
 const diaSemana = data.getDay();
 let diaSemanaTexto;
 
-if (diaSemana === 0) {
+switch (diaSemana) {
+case 0:
     diaSemanaTexto = 'Domingo';
-} else if (diaSemana === 1) {
+    break
+case 1:
     diaSemanaTexto = 'Segunda';
-}  else if (diaSemana === 2) {
+    break
+case 2:
     diaSemanaTexto = 'Terça';
-}  else if (diaSemana === 3) {
+    break
+case 3:
     diaSemanaTexto = 'Quarta';
-}  else if (diaSemana === 4) {
+    break
+case 4:
     diaSemanaTexto = 'Quinta';
-}  else if (diaSemana === 5) {
-    diaSemanaTexto = 'Sexta';
-}  else if (diaSemana === 6) {
-    diaSemanaTexto = 'Sabado';
+    break
+case 5:
+    diaSemanaTexto = 'Domingo';
+    break
+case 6:
+    diaSemanaTexto = 'Domingo';
+    break
+default:
+    diaSemanaTexto = '';
 }
+
+// if (diaSemana === 0) {
+//     diaSemanaTexto = 'Domingo';
+// } else if (diaSemana === 1) {
+//     diaSemanaTexto = 'Segunda';
+// }  else if (diaSemana === 2) {
+//     diaSemanaTexto = 'Terça';
+// }  else if (diaSemana === 3) {
+//     diaSemanaTexto = 'Quarta';
+// }  else if (diaSemana === 4) {
+//     diaSemanaTexto = 'Quinta';
+// }  else if (diaSemana === 5) {
+//     diaSemanaTexto = 'Sexta';
+// }  else if (diaSemana === 6) {
+//     diaSemanaTexto = 'Sabado';
+// } else {
+//     diaSemanaTexto = ''
+// }
 
 console.log(diaSemana, diaSemanaTexto)
