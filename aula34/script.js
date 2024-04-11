29a71893c7228ee174926996682bb4546af91902
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-11 18:27:44 -0300

    Update script.js

--- aula34/script.js
+++ aula34/script.js
@@ -1,4 +1,9 @@
-for (let i= 0; i <= 10; i++) {
-    const par = i % 2 === 0 ? 'par' : 'impar';
-    console.log(i, par)
+const frutas = [
+    'Maçã',
+    'Pera',
+    'Uva'
+]
+
+for (let i=0; i < frutas.length; i++) {
+    console.log(frutas[i])
 }
