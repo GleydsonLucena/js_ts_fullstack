084cc3395e2eb55dff2e98e93f79612e402b0c50
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-11 17:51:57 -0300

    Update script.js

--- aula32/script.js
+++ aula32/script.js
@@ -1,5 +1,4 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const [primeiroNumero, ,segundoNumero, ...resto] = numeros;
-console.log(primeiroNumero, segundoNumero);
-console.log(resto)
+const [um,  , tres,  , cinco] = numeros;
+console.log(um,tres,cinco);
