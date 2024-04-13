f5a89b17e20e576f1a4ce0f2b22fbaf3cd4fe6a5
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-12 21:59:15 -0300

    Update script.js

--- aula36/script.js
+++ aula36/script.js
@@ -1,4 +1,21 @@
-const frutas = ['Maçã', 'Banana', 'Abacaxi', 'Manga', 'Uva'];
-for (let i = 0; i<frutas.length; i++) {
-    console.log(frutas[i]);
+const pessoa = {
+    nome: 'Gleydson', 
+    sobrenome: 'Lucena', 
+    idade: 23
+};
+
+for (let i in pessoa) {
+    console.log(pessoa[i])
 }
+
+
+// for (let i = 0; i<frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+
+// FOR IN LER OS INDICE OU CHAVES
+
+// for (let i in frutas) {
+//     console.log(frutas[i])
+// }
+
