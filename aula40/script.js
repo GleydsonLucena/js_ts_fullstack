faed0e679668b45c6825bf9a36f98bd044cac441
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-15 18:55:18 -0300

    .

---
+++ aula40/script.js
@@ -0,0 +1,15 @@
+const pessoas = {
+    nome: 'Luiz', 
+    sobrenome: 'Otavio'
+};
+// for (let i in nome) {
+//     console.log(nome[i]);
+// }
+
+// for (let valor of nome) {
+//     console.log(valor);
+// }
+
+for (let i of pessoas) {
+    console.log(i);
+}
