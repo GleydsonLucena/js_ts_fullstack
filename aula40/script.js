bff3404bdc4a7330b86315bcb34b36db9b3a48a3
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-15 19:09:08 -0300

    Update script.js

--- aula40/script.js
+++ aula40/script.js
@@ -2,7 +2,7 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let numero of numeros) {
     if (numero === 3) {
-        break
     }
-    console.log(numero)
+    break
 }
+console.log(numero)
