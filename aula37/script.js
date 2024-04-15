43ec064635cc40457977790e3bcec2fba72c6b0c
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-15 17:58:27 -0300

    Create script.js

---
+++ aula37/script.js
@@ -0,0 +1,22 @@
+const pessoa = {
+    nome: 'Gleydson', 
+    sobrenome: 'Lucena', 
+    idade: 23
+};
+const chave = 'nome';
+console.log(pessoa[chaves])
+
+for (let i in pessoa) {
+    console.log(pessoa[i])
+}
+
+
+// for (let i = 0; i<frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+//  FOR IN LER OS INDICE OU CHAVES
+
+// for (let i in frutas) {
+//     console.log(frutas[i])
+// }
+
