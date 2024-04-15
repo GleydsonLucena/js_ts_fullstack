01afd40775540bb2fc8d1ab9803ca4b5a1282401
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-15 18:54:43 -0300

    Update script.js

--- aula39/script.js
+++ aula39/script.js
@@ -2,11 +2,19 @@ function random (min, max) {
     const r = Math.random() * (max - min) + max;
     return Math.floor(r);
 } 
+let min = 1;
+let max = 50;
+let rand = 10;
 
-let rand = random(1, 50);
+// while (rand !== 10) {
+//     rand = random(1, 50);
+//     console.log(rand)
+// }
 
-while (rand !== 10) {
-    rand = random(1, 50);
-    console.log(rand)
-}
+do {
+    rand = random(min, max);
+    console.log(rand);
+} while(rand !== 10);
+
+
 
