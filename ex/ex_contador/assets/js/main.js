c42340f6579faaa55150c09cfdab891d5a9003bb
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-16 20:37:55 -0300

    Update main.js

--- ex/ex_contador/assets/js/main.js
+++ ex/ex_contador/assets/js/main.js
@@ -4,12 +4,7 @@ const zerar = document.querySelector('#zerar');
 const contador = document.querySelector('.contador');
 
 inciar.addEventListener('click', (event)=>{
-    let contador1 = 0;
-    let numeros = `00:00:0`
    setInterval(()=> {
-    const data = new Date('00:00:00');
-    
-    
-    contador.innerHTML = data
+        const data = new Date('1:1:1970')
     }, 1000)
 })
