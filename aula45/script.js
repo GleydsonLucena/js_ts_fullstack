0adaa50619e08acc11c45fe1798938221f59aac2
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-16 19:07:45 -0300

    Update script.js

--- aula45/script.js
+++ aula45/script.js
@@ -1,7 +1,19 @@
+const retornaHora=(data)=> {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date.')
+    }
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR')
+}
 try {
-    // ESSA PARTE DO CÓDIGO É EXECULTADO QUANDO NÃO A ERROS
-} catch (err) {
-    // ESSE BLOCO É EXECULTADO QUANDO A ERROS
-} finally {
-    // SEMPRE
+
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora();
+    console.log(hora)
+} catch (e) {
+    console.log('erro')
 }
