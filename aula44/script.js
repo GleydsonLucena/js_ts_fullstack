19f1dda55d4e4136eaab9226856077232ea19a20
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-16 18:28:28 -0300

    Create script.js

---
+++ aula44/script.js
@@ -0,0 +1,14 @@
+const checkNumero = (num) => {
+    if (typeof num != 'number') {
+        return num
+    }
+    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz'
+    if (num % 3 === 0) return 'Fizz'
+    if (num % 5 === 0) return 'Buzz'
+    
+    return num;
+
+}
+for (let i = 0 ; i <= 100; i++) {
+    console.log(i, checkNumero(i))
+}
