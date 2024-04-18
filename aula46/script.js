41c7d851fc628036929390c90d1842021c057c69
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-17 22:21:01 -0300

    Update script.js

--- aula46/script.js
+++ aula46/script.js
@@ -8,7 +8,7 @@ const timer = setInterval(() => {console.log(mostrarHora())}, 1000)
 
 setTimeout(()=>{clearInterval(timer)}, 10000);
 
-setTimeout(()=>{console.log('Ola, Mundo!')}, 15000)
+setTimeout(()=>{console.log('Ola, Mundo!')}, 15000);
 
 
 
