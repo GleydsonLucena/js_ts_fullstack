8da0609900a8cd6ef4d53f8c72279c816dc5cae3
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-18 23:17:30 -0300

    Update script.js

--- aula50/script.js
+++ aula50/script.js
@@ -1,7 +1,13 @@
 // Argumentos fica salvo na variavel argumetns
-
-function funcao({ nome, sobrenome, idade }) {
-    console.log(nome, sobrenome, idade);
+const conta = function(operador, acumulador, ...numeros) {
+    for (let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -= numero;
+        if (operador === '*') acumulador *= numero;
+        if (operador === '/') acumulador /= numero;
+    }
+    
+    console.log(acumulador)
 }
-let obj = {nome: 'Gleydson', sobrenome: 'Lucena', idade: 20};
-funcao(obj)
+
+conta('+', 1, 20, 30, 40, 50);
