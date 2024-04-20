c84c7ff927a483d57513c82fbe4c079b1844b03f
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-20 16:24:53 -0300

    Create script.js

---
+++ aula56/script.js
@@ -0,0 +1,31 @@
+// FACTORY FUNCTION
+// fUNÇÃO CONSTRUTORA
+
+function criaPessoa(nome, sobrenome, a, p){
+    return {
+        nome, 
+        sobrenome,
+        fala(assunto) {
+            return `${nome} está falando ${assunto}.`
+        },
+        altura: a,
+        peso: p, 
+        // Getter
+        get imc() {
+            const cImc = this.peso / (this.altura ** 2);
+            return cImc.toFixed(2);
+        }, 
+        get nomeCompleto(valor) {return `${nome} ${sobrenome}`}
+
+        // Setter
+
+
+    };
+}
+
+const p1 = criaPessoa('Gleydson', 'Lucena', 1.8, 80);
+const p2 = criaPessoa('Maria', 'Joaquina', 1.6, 60);
+console.log(p1.fala('falando sobre JS'))
+console.log(p1.imc)
+console.log(p2.imc)
+console.log(p1.nomeCompleto('Gleydson lucena guimaraes'))
