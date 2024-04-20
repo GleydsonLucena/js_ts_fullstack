8d1690fcf80fa3977df9ccbd0887b9bbe27ff06c
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-19 21:44:36 -0300

    .

--- aula51/script.js
+++ aula51/script.js
@@ -14,14 +14,28 @@
 
 // console.log(criaPessoa('Gleydson', 'Lucena'));
 
-function falaFrase(comeco) {
-    function falaResto(resto) {
-        return  comeco + ' ' + resto
-    }
+// function falaFrase(comeco) {
+//     function falaResto(resto) {
+//         return  comeco + ' ' + resto
+//     }
 
-    return falaResto
+//     return falaResto
+// }
+
+// const olaMundo = falaFrase('Olá')
+
+// console.log(olaMundo('mundo'))
+
+function multiplicador(multi) {
+    return (n)=> {
+        return n * multi
+    };
 }
 
-const olaMundo = falaFrase('Olá')
+const duplica = multiplicador(2);
+const triplica = multiplicador(3);
+const quadriplica = multiplicador(4);
 
-console.log(olaMundo('mundo'))
+console.log(duplica(2))
+console.log(triplica(2))
+console.log(quadriplica(2))
