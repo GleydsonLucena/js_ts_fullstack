c600102b632305505cfa6309fe134fe7df248ac1
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-19 21:33:27 -0300

    Update script.js

--- aula51/script.js
+++ aula51/script.js
@@ -1,9 +1,27 @@
-function soma(a, b) {
-    return a + b
+// function soma(a, b) {
+//     return a + b;
+// }
+
+// function soma2(a, b) {
+//     console.log(a + b);
+// }
+
+// soma2(5, 2);
+
+// function criaPessoa(nome, sobrenome) {
+//     return {nome, sobrenome}
+// }
+
+// console.log(criaPessoa('Gleydson', 'Lucena'));
+
+function falaFrase(comeco) {
+    function falaResto(resto) {
+        return  comeco + ' ' + resto
+    }
+
+    return falaResto
 }
 
-function soma2(a, b) {
-    console.log(a + b)
-}
+const olaMundo = falaFrase('Olá')
 
-soma2(5, 2)
+console.log(olaMundo('mundo'))
