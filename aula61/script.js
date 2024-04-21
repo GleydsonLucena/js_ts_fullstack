e298e27aa87d871dc79259cdadad0c1de8e8ec41
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-21 14:10:52 -0300

    Update script.js

---
+++ aula61/script.js
@@ -0,0 +1,14 @@
+function* gerador1(){
+    yield 'Valor 1'
+    yield 'Valor 2'
+    yield 'Valor 3'
+    yield 'Valor 4'
+    yield 'Valor 5'
+    yield 'Valor 6'
+}
+
+const g1 = gerador1()
+
+for (let valor of g1) {
+    console.log(valor)
+}
