249f671e694cbdab781c2aefb2737a41b5ebe57c
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-21 19:27:59 -0300

    Update script.js

--- aula62/script.js
+++ aula62/script.js
@@ -1,5 +1,14 @@
-const nome = ['Eduardo', 'Maria', 'Joana'];
+// const nome = ['Eduardo', 'Maria', 'Joana', 'Joao', 'Maria'];
 
-nome.push('Gleydson');
+// const novo = nome.slice(1, 2)
 
-console.log(nome)
+// console.log(nome);
+
+const nome = 'Luiz Otavio Miranda';
+
+const novo = nome.split(' ');
+
+console.log(novo)
+
+const novos = novo.join(',');
+console.log(novos)
