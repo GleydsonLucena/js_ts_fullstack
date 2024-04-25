e60f625b53d9806e6d9b700ec5733120690a3705
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-24 21:18:41 -0300

    Update script.js

--- aula65/script.js
+++ aula65/script.js
@@ -1,12 +1,28 @@
-// Filter, map, reduce
+// Filter
+
+// SEMPRE RETORNA UM ARRAY COM A MESMA QUANTIDADE DE ELEMENTOS, OU MENOS.
 
 // RETORNE OS NÚMEROS MAIORES QUE 10
 
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-let arrayMaiores = numeros.filter((value, index) => {
-    console.log(value, index)
-    return (value > 10);
-}
-);
+// let arrayMaiores = numeros.filter((value, index) => {
+//     // console.log(value, index)
+//     return (value > 10);
+// }
+// );
 
+// RETORNE AS PESSOAS QUE TEM O NOME COM 5 LETRAS OU MAIS
+// RETORNE AS PESSOAS COM MAIS DE 50 ANOS
+// RETORNE AS PESSOAS CUJO NOME TERMINA COM A
+
+const pessoas = [
+    {nome:'Luiz', idade: 62},
+    {nome:'Maria', idade: 23},
+    {nome:'Eduardo', idade: 55},
+    {nome:'Letícia', idade: 19},
+    {nome:'Rosana', idade: 32},
+    {nome:'Wallace', idade: 47},
+]
+
+const pessoa = pessoas.filter(CSSMathValue)
