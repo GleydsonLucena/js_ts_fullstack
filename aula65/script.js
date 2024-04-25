976fed55e9f0c2a91db5af7761785afa3abbbe08
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-24 21:11:59 -0300

    Update script.js

--- aula65/script.js
+++ aula65/script.js
@@ -3,17 +3,10 @@
 // RETORNE OS NÚMEROS MAIORES QUE 10
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-let arrayMaiores = numeros.filter();
-let arrayMenores = [];
 
-for (let n of numeros) {
+let arrayMaiores = numeros.filter((value, index) => {
+    console.log(value, index)
+    return (value > 10);
+}
+);
 
-    if (n > 10) {
-        arrayMaiores.push(n);
-    } else {
-        arrayMenores.push(n);
-    }
-};
-
-console.log(arrayMaiores);
-console.log(arrayMenores);
