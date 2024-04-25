fd2a38c4bd4962e345b5ccd6895315aa67ac97dc
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-25 20:37:41 -0300

    Update script.js

--- aula66/script.js
+++ aula66/script.js
@@ -2,9 +2,27 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
 const numMaior = numeros.map( value => value * 2 );
 
-console.log(numMaior)
-console.log(numeros)
+// console.log(numMaior);
 
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 }
+]
+
+const nomes = pessoas.map( value => value.nome);
+
+const removerNome = pessoas.map( value => {
+    delete value['nome']
+    return value
+});
+
+console.log(removerNome);
+
+// console.log(nomes);
 
 
 
