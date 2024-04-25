0b9e9b84dcfb424c486a6a6cdd7813a70dd017ee
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-25 20:19:08 -0300

    Update script.js

--- aula66/script.js
+++ aula66/script.js
@@ -1,5 +1,10 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-const numMaior = numeros.map(value => value >= 50 ? console.log(value): false);
+const numMaior = numeros.map( value => value * 2 );
 
-console.log()
+console.log(numMaior)
+console.log(numeros)
+
+
+
+
