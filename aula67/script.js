f6c2df43704fd5d50e851347e989c33ef3aab4b7
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-25 21:24:51 -0300

    Update script.js

---
+++ aula67/script.js
@@ -0,0 +1,22 @@
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const soma = numeros.reduce((acumulador, valor, indice, array) => {
+    
+    return acumulador += valor
+}, 0)
+
+console.log(soma)
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 }
+]
+
+
+
+
+
