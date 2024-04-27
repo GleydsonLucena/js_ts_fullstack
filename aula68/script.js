974fc141506cde625fed5cef674a766356cb1f15
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-27 20:21:01 -0300

    Update script.js

--- aula68/script.js
+++ aula68/script.js
@@ -1,14 +1,9 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-const pares = numeros.filter(valor => valor % 2 === 0).reduce((acumulador, valor) => {
-    if (valor % 2 === 0) {
-        acumulador += valor;
-        console.log(`${acumulador} + ${valor}`)
-    }
-    return acumulador;
-}, 0);
-
-
+const pares = numeros
+.filter(valor => valor % 2 === 0)
+.map(valor => valor * 2)
+.reduce((acumulador, valor) => acumulador += valor, 0);
 
 console.log(pares)
 
