4829dcf2826b9e624ab85763bb42d57e12f459be
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-27 20:29:55 -0300

    Update script.js

--- aula69/script.js
+++ aula69/script.js
@@ -1,5 +1,12 @@
 const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+let cont = 0;
+while (true) {
+    cont++
+    console.log(cont)
+    if (cont === 8) {
+        break
+    }
+}
 
 
 // const pessoas = [
