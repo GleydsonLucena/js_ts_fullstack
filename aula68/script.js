79395015c969d6680c26e23bee8a057f73f47630
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-27 20:05:14 -0300

    Create script.js

---
+++ aula68/script.js
@@ -0,0 +1,16 @@
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 10 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 }
+]
+
+
+
+
