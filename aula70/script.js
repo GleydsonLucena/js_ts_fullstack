f0c8091784be22f834369c8cea0e567f4a69ce4a
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-27 20:30:25 -0300

    Create script.js

---
+++ aula70/script.js
@@ -0,0 +1,23 @@
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let cont = 0;
+while (true) {
+    cont++
+    console.log(cont)
+    if (cont === 8) {
+        break
+    }
+}
+
+
+// const pessoas = [
+//     { nome: 'Luiz', idade: 62 },
+//     { nome: 'Maria', idade: 23 },
+//     { nome: 'Eduardo', idade: 55 },
+//     { nome: 'Letícia', idade: 10 },
+//     { nome: 'Rosana', idade: 32 },
+//     { nome: 'Wallace', idade: 47 }
+// ]
+
+
+
+
