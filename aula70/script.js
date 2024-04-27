1021d6d4b3dfe65dfe5dbeeb7620f442e2467390
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-27 20:40:57 -0300

    Update script.js

--- aula70/script.js
+++ aula70/script.js
@@ -1,4 +1,20 @@
-const pessoa = {
-    nome: 'Gleydson',
-    sobrenome: 'Lucena'
-}
+//new Array(); []
+
+const array = new Array(1, 2, 3, 4, 5, 6);
+
+// new Object();
+
+const pessoa1 = new Object();
+pessoa1.nome = 'Gleydson';
+pessoa1.sobrenome = 'Lucena';
+
+const pessoa2 = new Object();
+pessoa2.nome = 'Luiz';
+pessoa2.sobrenome = 'Otavio';
+
+console.log(pessoa1);
+console.log(pessoa2);
+
+delete pessoa1.nome;
+
+console.log(pessoa1);
