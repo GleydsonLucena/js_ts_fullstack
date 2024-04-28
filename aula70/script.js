b0c261b82c0a5549f7121e7b5141424d5ac6881e
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-27 22:11:07 -0300

    Update script.js

--- aula70/script.js
+++ aula70/script.js
@@ -5,17 +5,31 @@ const array = new Array(1, 2, 3, 4, 5, 6);
 // new Object();
 
 const pessoa1 = new Object();
-pessoa1.nome = 'Gleydson';''
+pessoa1.nome = 'Gleydson'; ''
 pessoa1.sobrenome = 'Lucena';
 pessoa1.idade = 1;
-pessoa1.falarNome = function() {console.log(this.nome)};
-pessoa1.dataDeNasc = function() {
+pessoa1.falarNome = function () { console.log(this.nome) };
+pessoa1.dataDeNasc = function () {
     const dataAtual = new Date().getFullYear();
     return dataAtual - this.idade;
 }
 
 // criar objeto
 
-for(chave in pessoa1) {
-    console.log(pessoa1[chave])
+for (chave in pessoa1) {
+    // console.log(pessoa1[chave])
 }
+
+
+//modulos factory function, constructor functions, Classes
+
+function criaPessoa(nome, sobrenome) {
+    return {
+        nome,
+        sobrenome
+    }
+};
+
+const p1 = criaPessoa('Gleydson', 'Lucena');
+
+console.log(p1);
