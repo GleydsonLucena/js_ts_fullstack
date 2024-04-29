a0aa1bd3cf5e2fdedd801021daf89fc4d995be6d
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-29 19:31:02 -0300

    Update script.js

--- aula76/script.js
+++ aula76/script.js
@@ -6,7 +6,10 @@ function Produto(nome, preco)
     this.nome = nome;
     this.preco = preco;
 }
-Produto.prototype.aumento = (percent) => preco - (preco * (percent / 100)); 
 
-const p1 = new Produto
-console.log()
+Produto.prototype.aumento = function(percent) {return this.preco + (this.preco * percent / 100);}
+
+const p1 = new Produto('samisa', 10);
+p1.aumento(20);
+
+console.log(p1)
