053289bf5178f6136540a7e3b4c91a48292cf3d0
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-29 18:01:29 -0300

    Update script.js

---
+++ aula74/script.js
@@ -0,0 +1,22 @@
+/**
+O javaScript é baseado em protótipos para passar propriedades e métodos de um objeto para outro.
+
+Definição de protótipo 
+Protótipo é o termo usado para se referir ao que foi criado pela primeira vez, servindo de modelo ou molde para futuras produções.
+
+todos os objetos tem uma referência interna para um protótipo (_proto_) que vem da propriedade prototype da função contrutora que foi usada para criá-lo. Quando o tentamos acessar um membro no próprio objeto, primeiro o motor do JS vai tentar encontrar este membro e demais a cadeia de proótipos .
+*/
+
+
+// Contrutora = Molde (classe)
+function Pessoa (nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+}
+
+
+// Intância
+const p1 = new Pessoa('Gleydson', 'Lucena');
+
+console.log(p1);
