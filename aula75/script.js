a27e3aa3ca6f699f73db26ae6b7def6dc6183491
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-04-29 18:38:03 -0300

    Update script.js

--- aula75/script.js
+++ aula75/script.js
@@ -1,23 +1,16 @@
-// new Object -> Object.prototype
-
-const objA = 
+function Produto (nome, preco) 
 {
-    chaveA: 'A'
-    // _proto_ => Object.prototype
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function (percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+Produto.prototype.aumento = function (percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
 };
 
-const objB = 
-{
-    chaveB: 'B'
-    // _proto_ => Object.prototype
-};
-
-const objC = new Object();
-objC.chaveC = 'C';
-
-Object.setPrototypeOf(objB, objA)
-Object.setPrototypeOf(objC, objB)
-
-console.log(objA)
-console.log(objC)
-
+const p1 = new Produto('Camisa', 100);
+p1.aumento(100)
+console.log(p1)
