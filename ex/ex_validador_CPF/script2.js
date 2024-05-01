364c3fb8d91e654deece685e6375f975e56d696c
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-01 11:53:04 -0300

    .

---
+++ ex/ex_validador_CPF/script2.js
@@ -0,0 +1,17 @@
+// 705.484.450-52 070.987.720-03
+
+/*
+7x 0x 5x 4x 8x 4x 4x 5x 0x
+10 9  8  7  6  5  4  3  2 
+70 0  40 28 48 20 16 15 0 = 237
+
+11 - (237 % 11) = 5 (primeiro digito)
+se o número digitado for 9, consideramos 0.
+
+7x 0x 5x 4x 8x 4x 4x 5x 0x 5x
+11 10 9  8  7  6  5  4  3  2 
+77 0  45 32 56 24 20 20 0  10 = 284
+
+11 - (284 % 11) = 2 (segundo digito)
+se o número digitado for 9, consideramos 0.
+*/
