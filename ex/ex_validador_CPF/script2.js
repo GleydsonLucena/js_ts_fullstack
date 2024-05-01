dd8a73e834db06aef3e79a83cfb48c9ec5fb417c
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-01 14:29:45 -0300

    .

--- ex/ex_validador_CPF/script2.js
+++ ex/ex_validador_CPF/script2.js
@@ -14,4 +14,75 @@ se o número digitado for 9, consideramos 0.
 
 11 - (284 % 11) = 2 (segundo digito)
 se o número digitado for 9, consideramos 0.
+
+/^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}/
 */
+
+function CpfValidator(cpf) {
+    Object.defineProperty(this, 'cpfClean', {
+        enumerable: true,
+        get: function () {
+            return cpf.replace(/\D+/g, '');
+        }
+    });
+}
+
+CpfValidator.prototype.valid = function () {
+    if (typeof cpfClean === undefined) return false;
+    if (this.cpfClean.length !== 11) return false;
+    if (this.isSequence()) return false;
+
+    const cpfPartial = this.cpfClean.slice(0, -2);
+    const digitOne = this.CreateDigit(cpfPartial);
+    const digitTwo = this.CreateDigit(cpfPartial + digitOne);
+    const newCpf = cpfPartial + digitOne + digitTwo;
+    return newCpf === this.cpfClean;
+}
+
+CpfValidator.prototype.CreateDigit =
+    function (cpfPartial) {
+        const arrayDoCPF = Array.from(cpfPartial);
+        let regressiveCounter = arrayDoCPF.length + 1;
+        let sumArray = arrayDoCPF.reduce(
+            (ac, valor) => {
+                ac += (regressiveCounter * Number(valor));
+                regressiveCounter--
+                return ac;
+            }, 0)
+
+        const digit = 11 - (sumArray % 11);
+        return digit > 9 ? '0' : String(digit);
+    }
+
+CpfValidator.prototype.isSequence = function () {
+    const sequence = this.cpfClean[0].repeat(this.cpfClean.length);
+    return sequence === this.cpfClean;
+}
+
+
+
+// FUNÇÕES SEPARADAS
+
+const appendValidation=(cpf, validator)=> {
+    const cpfInformed = document.querySelector('.cpf-informed');
+    const result = document.querySelector('.result');
+
+    
+}
+
+document.addEventListener('click', 
+e => {
+    const el = e.target;
+     
+    if(el.classList.contains('btn')){
+        if (!txtCpf) alert('Digite o CPF!')
+        if (this.cpfClean > 11) alert('Digite um CPF válido!')
+
+        const cpf = document.querySelector('#txtCpf').value;
+        const validator = new CpfValidator(cpf);
+
+        appendValidation(cpf, validator.valid())
+    };
+});
+
+
