86ec8d2450b69ed788318d8d79cff22c6b3fc455
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-07 19:26:27 -0300

    Update script.js

--- aula81/script.js
+++ aula81/script.js
@@ -1,36 +1,60 @@
-const _velocidade = Symbol('velocidade')
+// const _velocidade = Symbol('velocidade')
 
-class Carro {
-    constructor(nome) {
+// class Carro {
+//     constructor(nome) {
+//         this.nome = nome;
+//         this[_velocidade] = 0;
+//     }
+
+//     get velocidade() {
+//         return this[_velocidade];
+//     }
+
+//     set velocidade(valor) {
+//         if (typeof valor !== 'number') return;
+//         if (valor >= 100 || valor <= 0) return;
+//         this[_velocidade] = valor
+//     }
+
+//     acelerar() {
+//         if (this[_velocidade] >= 100) return;
+//         this[_velocidade]++;
+
+//     }
+
+//     freiar() {
+//         if (this[_velocidade] <= 0) return;
+//         this[_velocidade]--;
+//     }
+
+// }
+
+// const c1 = new Carro('Fusca');
+
+// c1.velocidade = 99;
+
+// console.log(c1)
+
+class Pessoa {
+    constructor (nome, sobrenome) {
         this.nome = nome;
-        this[_velocidade] = 0;
+        this.sobrenome = sobrenome;
     }
 
-    get velocidade() {
-        return this[_velocidade];
+    get nomeCompleto() {
+        return this.nome + ' ' + this.sobrenome;
     }
 
-    set velocidade(valor) {
-        if (typeof valor !== 'number') return;
-        if (valor >= 100 || valor <= 0) return;
-        this[_velocidade] = valor
+    set nomeCompleto(valor) {
+        valor = valor.split(' ');
+        this.nome = valor.shift();
+        this.sobrenome = valor.join(' ')
+    
     }
-
-    acelerar() {
-        if (this[_velocidade] >= 100) return;
-        this[_velocidade]++;
-
-    }
-
-    freiar() {
-        if (this[_velocidade] <= 0) return;
-        this[_velocidade]--;
-    }
-
 }
 
-const c1 = new Carro('Fusca');
+const p1 = new Pessoa('Gleydson', 'Lucena');
 
-c1.velocidade = 99;
+p1.nomeCompleto = 'Luiz Miranda'
 
-console.log(c1)
+console.log(p1.nomeCompleto, '#', p1.nome, '#', p1.sobrenome)
