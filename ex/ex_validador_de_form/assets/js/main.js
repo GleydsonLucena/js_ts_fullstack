9bc7ffa85503b039e02ed4568f990db24f58f2dd
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-12 12:44:44 -0300

    Autalização

--- ex/ex_validador_de_form/assets/js/main.js
+++ ex/ex_validador_de_form/assets/js/main.js
@@ -1,30 +1,5 @@
-class ValidadorForm {
-    constructor (nome, sobrenome, usuario) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.usuario = usuario;
-    }
-    mostra() {
-        console.log(this.nome)
-    }
-
-    criaP() {
-        const p = document.createElement('p');
-        p.classList.add('fraseErro');
-        return p;
-    }
-
-    indentDiv(classDiv) {
-        const div = document.querySelector(`.${classDiv}`);
-        return div;
-    }
-
-    inputClean(input) {
-        const inputClean = `${input}`;
-        return inputClean;
+class ValidarFormulário {
+    constructor () {
+        this.formulario = document.querySelector('.form');
     }
 }
-
-const valid = new ValidadorForm(txtNome , 'sobrenome', 'usu');
-
-
