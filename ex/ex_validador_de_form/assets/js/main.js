3530a667ff042ffaf7c4343a815a60ba40676f4c
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-12 12:34:37 -0300

    atualização

--- ex/ex_validador_de_form/assets/js/main.js
+++ ex/ex_validador_de_form/assets/js/main.js
@@ -1,2 +1,30 @@
+class ValidadorForm {
+    constructor (nome, sobrenome, usuario) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.usuario = usuario;
+    }
+    mostra() {
+        console.log(this.nome)
+    }
+
+    criaP() {
+        const p = document.createElement('p');
+        p.classList.add('fraseErro');
+        return p;
+    }
+
+    indentDiv(classDiv) {
+        const div = document.querySelector(`.${classDiv}`);
+        return div;
+    }
+
+    inputClean(input) {
+        const inputClean = `${input}`;
+        return inputClean;
+    }
+}
+
+const valid = new ValidadorForm(txtNome , 'sobrenome', 'usu');
 
 
