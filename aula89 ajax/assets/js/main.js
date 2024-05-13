baa71ebdfaf48b536ae0f8b2b1661607011ef12b
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-13 20:49:14 -0300

    aula 89 ajax

--- aula89 ajax/assets/js/main.js
+++ aula89 ajax/assets/js/main.js
@@ -1,16 +1,19 @@
 const request = obj => {
-    const xhr = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
     xhr.open(obj.method, obj.url, true);
     xhr.send();
 
     xhr.addEventListener('load',
         () => {
             if (xhr.status >= 200 && xhr.status < 300) {
-                obj.success(xhr.responseText);
+                resolve(xhr.responseText);
             } else {
-                obj.error(xhr.statusText)
+                reject(xhr.statusText)
             }
         });
+    })
+    
 };
 
 
@@ -26,18 +29,18 @@ document.addEventListener('click',
     }
 )
 
-function carregaPafina(el) {
+async function carregaPafina(el) {
     const href = el.getAttribute('href');
-    request({
-        method: 'GET',
-        url: href,
-        success(response) {
-            loadResult(response);
-        },
-        error(errorText) {
-            console.log(errorText)
-        }
-    })
+    try {
+        const response = await request({
+            method: 'GET',
+            url: href,
+        });
+        loadResult(response);
+    } catch(e) {
+        console.log(e)
+    }
+    
 }
 
 function loadResult(response) {
