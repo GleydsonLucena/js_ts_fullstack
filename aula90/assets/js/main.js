0076faf8ddc1702faff28b846e2b8e48a0f3e1f6
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-14 18:48:04 -0300

    Usando fetch

---
+++ aula90/assets/js/main.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('click',
+    e => {
+        const el = e.target;
+        const tag = el.tagName.toLowerCase();
+
+        if (tag === 'a') {
+            e.preventDefault();
+            carregaPafina(el)
+            loadResult()
+        }
+    }
+)
+
+async function carregaPafina(el) {
+    const href = el.getAttribute('href');
+    await fetch(href)
+        .then(resp => {
+            if (resp.status !== 200) throw new Error('DEu ruim!');
+            return resp.text()
+                .then(html => {
+                    loadResult(html);
+                })
+        })
+        ;
+
+}
+
+function loadResult(html) {
+    const div = document.querySelector('.resultado');
+    div.innerHTML = html;
+}
+
+
+// fetch('pagina.html')
+//     .then(resp => {
+//         if (resp.status !== 200) throw new Error('ERRO 404 NOSSO!');
+//         return resp.text();
+//     })
+//     .then(html => console.log(html))
+//     .catch(e => console.error(e));
