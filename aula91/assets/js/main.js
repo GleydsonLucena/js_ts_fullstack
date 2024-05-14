b58130b39b03898931b0da8a26e0956caf2eba94
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-14 19:32:46 -0300

    Usando JSON

--- aula91/assets/js/main.js
+++ aula91/assets/js/main.js
@@ -4,12 +4,34 @@ fetch('pessoas.json')
         return resp.json();
     })
     .then(json => {
-        for (let i = 0; i < json.length; i++) {
-            if (json[i].nome === pessoa) {
-                console.log(json[i])
-            }
-        }
-    }
-    )
+        loadJson(json);
+    });
 
+function loadJson(json) {
+    const table = document.createElement('table');
+
+    const divJson = document.querySelector('.json-result');
+
+    for (let pessoa of json) {
+        const tr = document.createElement('tr');
+
+        let td = document.createElement('td');
+        td.innerHTML = pessoa.nome;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = pessoa.cpf;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = pessoa.idade;
+        tr.appendChild(td);
+
+
+        table.appendChild(tr)
+    }
+
+    divJson.appendChild(table);
+
+}
 
