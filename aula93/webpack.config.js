b25a9a0310c5308ef44bb0addf2b96bc00480190
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-16 19:13:20 -0300

    Configurando o Webpack

--- aula93/webpack.config.js
+++ aula93/webpack.config.js
@@ -1,3 +1,27 @@
+const { map } = require('core-js/core/array');
 const path = require('path'); //CommonJS
+const { config } = require('process');
+const { sources } = require('webpack');
 
 
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/, 
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }]
+    },
+    devtool: 'source-map'
+}
