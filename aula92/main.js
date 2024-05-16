095b62b045789eaf1790b1a84fb4dfc5a345d5dd
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-16 18:37:01 -0300

    Testando babel

--- aula92/main.js
+++ aula92/main.js
@@ -1,58 +1,12 @@
-function rand(min, max) {
-    min *= 1000;
-    max *= 1000;
-    return Math.floor(Math.random() * (max - min) + min);
-}
+const nome = 'Luiz';
+const obj = { nome };
+const novoObj = { ...obj };
 
-function esperaAi(msg, tempo) {
-    return new Promise((resolve, reject) => {
+console.log(novoObj);
 
-        setTimeout(e => {
-            if (typeof msg !== 'string') {
-                reject('Deu ruim');
-                return;
-            }
-
-            resolve(msg.toUpperCase());
-            return;
-        }, tempo)
-    })
-}
-
-async function executa() {
-    try {
-        const Fase1 = await esperaAi('Fase 1', rand(0, 3));
-        console.log(Fase1);
-        const Fase2 = await esperaAi('Fase 2', rand(0, 3));
-        console.log(Fase2);
-        const Fase3 = await esperaAi('Fase 3', rand(0, 3));
-        console.log(Fase3);
-    } catch(e) {
-        console.log(e)
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
     }
-
-};
-
-/*
-Pending - pendente
-fullField - resolvida
-reject - rejeitada
-*/
-
-
-
-executa();
-
-// esperaAi('Fase 1', rand(0, 3))
-//     .then(valor => {
-//         console.log(valor)
-//         return esperaAi('Fase 2', rand(0, 3))
-//     })
-//     .then(valor => {
-//         console.log(valor);
-//         return esperaAi('Fase 3', rand(0, 3))
-//     })
-//     .then(valor=> {
-//         console.log(valor);
-//     })
-
+}
