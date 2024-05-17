c8096d9367de3c641e224c08cf27a61c08c255f2
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-17 18:32:31 -0300

    .

--- aula94/src/index.js
+++ aula94/src/index.js
@@ -1,3 +1,4 @@
-import { nome, sobrenome, idade, soma } from './modulo1';
+import { nome , sobrenome, idade, soma } from './modulo1';
 
-console.log(nome, sobrenome, idade, soma(3, 2))
+
+console.log(nome2, nome, sobrenome, idade, soma(3, 2))
