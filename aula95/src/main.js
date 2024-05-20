5a7a47e68a38827621ba6eb931221d553254961d
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-20 20:25:01 -0300

    .

--- aula95/src/main.js
+++ aula95/src/main.js
@@ -1,10 +1,8 @@
 import GeraCPF from "./modules/GeraCPF";
 import './assets/css/style.css';
 
-function append() {
-    const cpf = new GeraCPF();
-    const div = document.querySelector('.cpf-gerado');
-    div.innerHTML = cpf.geraNovoCpf()
+function oi () {
+    const novoCPF = new GeraCPF();
+    console.log(novoCPF.geraNovoCpf())
 }
-append()
-
+oi()
--- aula95/src/modules/GeraCPF.js
+++ aula95/src/modules/GeraCPF.js
@@ -2,19 +2,15 @@ import ValidatorCPF from "./ValidatorCPF";
 
 
 export default class GeraCPF {
-    constructor() {
-        this.digito = new ValidatorCPF();
-    }
     rand(min= 100000000, max=999999999) {
         return String(math.floor(math.random() * (max-min) + min));
     }
 
     geraNovoCpf() {
-        const cpfSemDigito  = this.rand();
-        const digito1 = this.digito.createDigit(cpfSemDigito);
-        const digito2 = this.digito.createDigit(cpfSemDigito + digito1);
-        const novoCPF = cpfSemDigito + digito1 + digito2;
-        return 'oi';
+        return console.log(this.rand())
+        // const digito2 = this.digito.createDigit(cpfSemDigito + digito1);
+        // const novoCPF = cpfSemDigito + digito1 + digito2;
+        
     }
 }
 
