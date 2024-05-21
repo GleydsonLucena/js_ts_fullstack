ebdeb1d718bf8bb7bc26b8fb4e7caf57bc3e23a3
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-21 19:03:06 -0300

    .

--- aula96_gerador-de-senha/src/main.js
+++ aula96_gerador-de-senha/src/main.js
@@ -1,2 +1,6 @@
 import './assets/css/style.css';
 
+import { caracters, maiusculas, minusculas, num, simbolos } from './modules/checked';
+
+const 
+
---
+++ aula96_gerador-de-senha/src/modules/checked.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('.form').addEventListener('submit', e => {
+    e.preventDefault()
+})
+
+export const num = document.querySelector('#num').checked;
+export const maiusculas = document.querySelector('#maiusculas').checked;
+export const minusculas = document.querySelector('#minusculas').checked;
+export const simbolos = document.querySelector('#simbolos').checked;
+export const caracters = document.querySelector('#qnt-caracters');
+
+
+
