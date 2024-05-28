d7c34482a5122e19b9dcc94fcdd76163d380a014
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-28 17:53:46 -0300

    Update server.js

--- node/aula6/server.js
+++ node/aula6/server.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 
 
-.get
+// https://meusite.com/ <-GET - > entrega a página
+
+app.get('/', (req, res) => {
+    req.listeners(res)
+    res.send(('Hellow World!'))
+})
