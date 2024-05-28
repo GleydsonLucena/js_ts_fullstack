4c33f796589fa84406cb89f429e3bba0a1f7ebbb
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-28 18:04:15 -0300

    Update server.js

--- node/aula6/server.js
+++ node/aula6/server.js
@@ -5,6 +5,14 @@ const app = express();
 // https://meusite.com/ <-GET - > entrega a página
 
 app.get('/', (req, res) => {
-    req.listeners(res)
     res.send(('Hellow World!'))
 })
+
+app.get('/contato', (res, req) => {
+    res.send('Obrigado em entrar em contato com a gente!')
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor execultando na porta 3000');
+});
