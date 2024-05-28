916310ff96e072beec4438fcf2e285ffc255cdd0
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-27 22:11:53 -0300

    Update index.js

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -3,22 +3,27 @@ const path = require('path');
 
 async function readdir(rootDir) {
     rootDir = rootDir || path.resolve(__dirname);
-    const files = await fs.readdir(__dirname);
+    const files = await fs.readdir(rootDir);
     walk(files, rootDir);
 }
 
-async function walk(files , rootDir) {
+async function walk(files, rootDir) {
     for (let file of files) {
-        const fileFullPath = path.resolve(rootDir, file)
+        const fileFullPath = path.resolve(rootDir, file);
         const stats = await fs.stat(fileFullPath);
+
+        if (/\.git/g.test(fileFullPath)) continue;
+        if (/node_modules/g.test(fileFullPath)) continue;
 
         if (stats.isDirectory()) {
             readdir(fileFullPath);
             continue;
         }
 
-        console.log(fileFullPath, stats.isDirectory());
+        if (
+            !/\.html$/g.test(fileFullPath)
+        ) continue;
+        console.log(fileFullPath);
     }
 }
-
-readdir('/home/gleydson/Documentos/GitHub/cursoJS-TS-fullstack/');
+readdir('/home/gleydson/Documentos/GitHub/cursoJS-TS-fullstack');
