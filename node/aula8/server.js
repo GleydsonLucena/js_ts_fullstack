88639574ce6231a113a8f833aa944f251eeeb0da
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-29 17:33:38 -0300

    .

--- node/aula8/server.js
+++ node/aula8/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+
+// https://meusite.com/ <-GET - > entrega a página
+
 app.get('/',
     (req, res) => {
         res.send(`
@@ -17,6 +20,10 @@ app.post('/',
         res.send('Recebi o formulário!')
     })
 
+app.get('/contato',
+    (req, res) => {
+        res.send('Obrigado por enviar!')
+    })
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
     console.log('Servidor execultando na porta 3000');
