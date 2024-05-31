1796d3495018155f1ccbc7460a8ef66a02ae5465
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-31 18:45:47 -0300

    aula13

---
+++ node/aula13/src/controllers/homeController.js
@@ -0,0 +1,14 @@
+exports.paginaInicial =
+    (req, res) => {
+        res.render('index')
+    }
+
+exports.trataPost =
+    (req, res) => {
+        res.send('Ei sou sua nova rota de POST')
+    }
+
+    exports.rotaContato = 
+    (req, res) => {
+        res.send('Olá está é sua página de contato!')
+    }
