6c3261dcc6a2c30c9a668d4ff996d94d0c869595
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-04 19:23:54 -0300

    .

---
+++ node/aula15/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(resp => app.emit('OK!'))
+    .catch(e => console.log(e));
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+// https://meusite.com/ <-GET - > entrega a página
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Nossos próprios middlewares
+
+app.use(middlewareGlobal);
+app.use(routes)
+
+app.on('OK!', () => {
+    app.listen(3000,
+        () => {
+            console.log('Acessar http://localhost:3000');
+            console.log('Servidor execultando na porta 3000');
+        });
+});
---
+++ node/aula15/src/controllers/contatoController.js
@@ -0,0 +1,4 @@
+exports.rotaContato = 
+(req, res) => {
+    res.send('Olá está é sua página de contato!')
+}
