7e3dc7a2c09e561d527feb891269f2c148b60916
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-08 18:25:31 -0300

    Update server.js

--- node/aula17/server.js
+++ node/aula17/server.js
@@ -1,17 +1,25 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
 const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
+
 const session = require('express-session');
+
 const MongoStore = require('connect-mongo');
+
 const flash = require('connect-flash');
+
 const routes = require('./routes');
+
 const path = require('path');
+
 const helmet = require('helmet');
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
