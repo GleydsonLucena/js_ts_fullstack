da61fa3d7cd281477d4e116edde8f6dcb49da46d
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-09 13:30:23 -0300

    ,

--- node/aula14/src/controllers/homeController.js
+++ node/aula14/src/controllers/homeController.js
@@ -1,9 +1,11 @@
 const HomeModel = require('..models/HomeModel')
 
-HomeModel.create( { 
-    titulo: 'Um titulo de testes', 
-    descricao:  'Uma descricao de testes'
-}).then(dados=>;
+HomeModel.create({
+    titulo: 'Um titulo de testes',
+    descricao: 'Uma descricao de testes'
+})
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e));
 
 exports.paginaInicial =
     (req, res) => {
