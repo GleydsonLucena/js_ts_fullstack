2a57ded544ec0d2c04ac92847b1a38d3e44c0a2c
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-13 20:33:17 -0300

    estllizando componentes

--- react/lista/src/components/Main.js
+++ react/lista/src/components/Main.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
+import "./Main.css";
+
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    novaTarefa: '',
+  };
 
-    this.state = {
-      novaTarefa: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
+  handleChange=(event)=> {
     this.setState({ novaTarefa: event.target.value });
   }
 
@@ -19,7 +15,7 @@ export default class Main extends Component {
     const { novaTarefa } = this.state;
     return (
       <div className="main">
-        <h1>{ novaTarefa }</h1>
+        <h1>Lista de tarefas</h1>
 
         <form action="#" onSubmit={this.handleSubmit}>
           <input value={this.state.novaTarefa} onChange={this.handleChange} type="text" />
