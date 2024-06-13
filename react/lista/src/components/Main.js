e627aa24a7db43c87e83cce6a36bc04b11937df8
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-13 20:18:09 -0300

    Update Main.js

--- react/lista/src/components/Main.js
+++ react/lista/src/components/Main.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      novaTarefa: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ novaTarefa: event.target.value });
+  }
+
   render() {
-    return <h1>Meu primeiro componente com estado!</h1>
+    const { novaTarefa } = this.state;
+    return (
+      <div className="main">
+        <h1>{ novaTarefa }</h1>
+
+        <form action="#" onSubmit={this.handleSubmit}>
+          <input value={this.state.novaTarefa} onChange={this.handleChange} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    )
   }
 }
