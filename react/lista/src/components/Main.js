b644cf467f72664dbb1166ecc369f9dafeb1322b
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-17 17:59:43 -0300

    .

--- react/lista/src/components/Main.js
+++ react/lista/src/components/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "./Main.css";
 
+//form
+
+
 
 export default class Main extends Component {
   state = {
@@ -12,6 +15,7 @@ export default class Main extends Component {
   }
 
   render() {
+    // eslint-disable-next-line no-unused-vars
     const { novaTarefa } = this.state;
     return (
       <div className="main">
@@ -19,7 +23,8 @@ export default class Main extends Component {
 
         <form action="#" onSubmit={this.handleSubmit}>
           <input value={this.state.novaTarefa} onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+          <button type="submit">
+          </button>
         </form>
       </div>
     )
