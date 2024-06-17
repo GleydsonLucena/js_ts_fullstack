df0404555e221e3cfbf180eee69aebf3bbb8f7c4
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-17 18:34:56 -0300

    Colocando icons no components

--- react/lista/src/components/Main.js
+++ react/lista/src/components/Main.js
@@ -1,24 +1,36 @@
 import React, { Component } from "react";
 import "./Main.css";
 
+//form
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShareFromSquare } from '@fortawesome/free-solid-svg-icons'
+
+
 export default class Main extends Component {
   state = {
     novaTarefa: '',
   };
 
-  handleChange=(event)=> {
+  handleChange = (event) => {
     this.setState({ novaTarefa: event.target.value });
   }
 
   render() {
     const { novaTarefa } = this.state;
+
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" onSubmit={this.handleSubmit}>
-          <input value={this.state.novaTarefa} onChange={this.handleChange} type="text" />
-          <button type="submit"> Enviar
+        <form action="#" onSubmit={this.handleSubmit} className="form">
+          <input
+            placeholder="Digite algo:"
+            value={novaTarefa}
+            onChange={this.handleChange}
+            type="text" />
+          <button type="submit">
+            Enviar <FontAwesomeIcon icon={faShareFromSquare} />
           </button>
         </form>
       </div>
